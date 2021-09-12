649abb3fa9bd28af78d3fdb176c621067363960b
Alexander Shkurlatov <shkurlatov80@gmail.com>
2021-09-13 04:48:27 +1200

    Add project files.

---
+++ TraceProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TraceProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
